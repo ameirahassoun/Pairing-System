008c8bc3c323eaa5d449d443c1d3cf0d85e97908
ameirahassoun <afhassoun5@gmail.com>
2018-12-08 22:13:58 +0200

    add axios to the compnent did mount and get data from the server

--- system/src/Components/history.js
+++ system/src/Components/history.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class History extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+        history:[]
+    }
+  }
+
+  componentWillMount(){
+
+  axios.get('/gethistory')
+    .then(({data}) => {
+      this.setState({history: data})
+    })
+    .catch(err => {
+      throw err;
+    })
+  }
   render() {
     return (
       <div>
