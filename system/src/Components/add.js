ed3154d1f4125910af509bf31e20f3f63d6dc274
ameirahassoun <afhassoun5@gmail.com>
2018-12-08 22:13:06 +0200

    add state to the addstudent

--- system/src/Components/add.js
+++ system/src/Components/add.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
 
 class Add extends Component {
-  render() {
-    return (
-      <div>
-        Add
-      </div>
-    )
-  }
+    constructor(props) {
+        super(props);
+        this.state={
+        studentname:'',
+        level:'',
+      }
+    }
+
+    handleChange = e =>  {
+        this.setState({
+          [e.target.name] :e.target.value
+        });
+      };
+
+    render() {
+        return (
+        <div>
+            <input type='text'
+                name='studentname'
+                placeholder='student name'
+                onChange={this.handleChange}
+                />
+                <input type='number'
+                name='level'
+                placeholder='level'
+                max='5'
+                min='0'
+                onChange={this.handleChange}
+                />
+                <button onClick={this.addstudent}>send</button>
+        
+                <hr />
+        </div>
+        )
+    }
 }
 
 export default Add;
