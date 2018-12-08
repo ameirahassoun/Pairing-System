6d6f6bd4863d0ec0ca58e0738f4910beb64fda3b
ameirahassoun <afhassoun5@gmail.com>
2018-12-08 22:36:59 +0200

    get all studnet from the server

--- system/src/Components/pairing.js
+++ system/src/Components/pairing.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Pairing extends Component {
-  render() {
-    return (
-      <div>
-        Pairing
-      </div>
-    )
-  }
+
+  componentWillMount() {
+    axios.get('/getAllStudents')
+      .then(data => {
+          if (!data.length % 2 === 0) {
+            data.push({
+              studentName: 'alone'
+            })
+          }
+          this.setState({
+            allStudents: data
+          })
+        })
+      .catch(err => {
+          console.log('failed to add student')
+
+      })  
+  };
+
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
 }
 
 export default Pairing;
