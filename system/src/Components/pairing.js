87c27b80e0078c2bfb87ede79d07ca319d704bfa
ameirahassoun <afhassoun5@gmail.com>
2018-12-08 23:10:06 +0200

    add pair algrthim

--- system/src/Components/pairing.js
+++ system/src/Components/pairing.js
@@ -28,8 +28,56 @@ class Pairing extends Component {
             })
         .catch(err => {
             console.log('failed to add student')
+            throw err;
         })  
     };
+
+    pair = () => {
+        let pairsArray = [];
+        let checkedArray = [];
+        let {allStudents} = this.state;
+        let randomNum = 0;
+        let firstStudentsArray = [];
+        let secondStudentsArray = [];
+        while(checkedArray.length<allStudents.length){
+          randomNum = Math.floor(Math.random() * allStudents.length) + 0;
+          if(!checkedArray.includes(randomNum)){
+            checkedArray.push(randomNum);
+          }
+        }
+      
+        for(let i=0;i<checkedArray.length;i=i+2){
+          let j = allStudents[checkedArray[i]].studentName;
+          let z = allStudents[checkedArray[i+1]].studentName;
+          pairsArray.push([j,z]);
+        }
+        for (let i = 0; i < pairsArray.length; i++) {
+          firstStudentsArray.push(pairsArray[i][0]);
+          secondStudentsArray.push(pairsArray[i][1]);
+        }
+        this.setState({
+          firstStudentsArray: firstStudentsArray,
+          secondStudentsArray: secondStudentsArray,
+          pairsArray: pairsArray,
+          show: true
+        })
+      }
+    
+
+    save = () => {
+        const { firstStudentsArray , secondStudentsArray } = this.state;
+        axios.post('/history/addHistory',
+        {
+            firstStudentsArray: firstStudentsArray,
+            secondStudentsArray: secondStudentsArray
+          })
+          .then(() => {
+              this.addToPairs();
+          }) 
+          .catch(err => {
+              throw err;
+          }) 
+      }
 
     addToPairs = () => {
         let { pairsArray } = this.state ;
