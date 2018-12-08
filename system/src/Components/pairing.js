af27eb0c67ec6ae9dc6d4c4b3943f7c456a5b748
ameirahassoun <afhassoun5@gmail.com>
2018-12-08 23:02:50 +0200

    add pairing function

--- system/src/Components/pairing.js
+++ system/src/Components/pairing.js
@@ -2,30 +2,69 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class Pairing extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          show: false,
+          allStudents: [],
+          firstStudentsArray: [],
+          secondStudentsArray: [],
+          pairsArray: [],
+          pairsToShow: []
+        }
+      }
 
-  componentWillMount() {
-    axios.get('/getAllStudents')
-      .then(data => {
-          if (!data.length % 2 === 0) {
-            data.push({
-              studentName: 'alone'
+    componentWillMount() {
+        axios.get('/getAllStudents')
+        .then(data => {
+            if (!data.length % 2 === 0) {
+                data.push({
+                studentName: 'alone'
+                })
+            }
+            this.setState({
+                allStudents: data
             })
-          }
-          this.setState({
-            allStudents: data
-          })
-        })
-      .catch(err => {
-          console.log('failed to add student')
-      })  
-  };
+            })
+        .catch(err => {
+            console.log('failed to add student')
+        })  
+    };
 
+    addToPairs = () => {
+        let { pairsArray } = this.state ;
+        
+        if (pairsArray[pairsArray.length - 1].studentName === 'alone') {
+        pairsArray.pop();
+        }
+        axios.put('/editpair', {
+            pairsArray: pairsArray
+        })
+        .then(() => {
+            console.log('saved');
+
+        }) 
+        .catch (() => {
+            console.log('failed')
+        });
+    }
 
     render() {
         return (
-            <div>
-                
+        <div>
+            <button onClick={this.pair.bind(this)}>Pair</button>
+            <button onClick={this.save.bind(this)}>save</button>
+            <hr />
+            {
+            this.state.show && <div>
+                {
+                this.state.pairsArray.map(function (pair, i) {
+                    return <h3 key={i}> {pair[0]}  WITH  {pair[1]}</h3>
+                })
+                }
             </div>
+            }
+        </div>
         )
     }
 }
